c1e353a94c25132934f23572ca70c823eced51ea
Eugene Marcotte <emarcotte@gmail.com>
2017-04-22 19:49:24 -0400

    inputs

--- src/Chopper.js
+++ src/Chopper.js
@@ -4,6 +4,7 @@ LD38.Chopper = LD38.Note.extend({
 	init: function(x) {
 		this._super(LD38.Note, 'init', [x, 40, {
 			image: "chopper",
+			key: "up",
 		}]);
 	},
 });
--- src/Hoop.js
+++ src/Hoop.js
@@ -4,6 +4,7 @@ LD38.Hoop = LD38.Note.extend({
 	init: function(x) {
 		this._super(LD38.Note, 'init', [x, 75, {
 			image: "hoops",
+			key: "right",
 		}]);
 	},
 });
